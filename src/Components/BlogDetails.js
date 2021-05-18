39f02f2e6ac97e5598cf5a237f8022fb1a3e43d9
tareqferdous <16101017@uap-bd.edu>
2021-05-18 16:39:08 +0600

    css change

--- src/Components/BlogDetails.js
+++ src/Components/BlogDetails.js
@@ -4,8 +4,8 @@ import { Button, Card } from 'react-bootstrap';
 const BlogDetails = ({blog}) => {
     return (
        
-            <div className="col-md-4 pt-3  portfolio-info mx-auto">
-            <img className="img-fluid pb-3 effect" src={blog.pic} alt=""/>
+            <div className="col-md-4 effect pt-3  portfolio-info mx-auto">
+            <img className="img-fluid pb-3 " src={blog.pic} alt=""/>
             <h4 style={{color: '#fff'}} className="text-center pb-2">{blog.title}</h4>
             <p className="text-center text-secondary" style={{color: '#F8F8FF', fontSize: '16px'}}>{blog.description} <a href={blog.live} style={{color: 'royalblue', textDecoration: 'none', fontSize: '18px'}} target="_blank">Read More</a></p>
                 <div class="card-footer bg-dark text-center">
--- src/Components/PortfolioDetails.js
+++ src/Components/PortfolioDetails.js
@@ -4,12 +4,12 @@ import { Button, Card, CardDeck } from 'react-bootstrap';
 const PortfolioDetails = ({details}) => {
     return (
         
-        <div className="col-md-4 pt-3  portfolio-info">
-        <img className="img-fluid pb-3 effect" src={details.pic} alt=""/>
+        <div className="col-md-4 pt-3 effect portfolio-info">
+        <img className="img-fluid pb-3 " src={details.pic} alt=""/>
         <h4 style={{color: '#fff'}} className="text-center pb-2">{details.title}</h4>
         <p className="text-center text-secondary" style={{color: '#F8F8FF', fontSize: '16px'}}>{details.description}</p>
         <div class="card-footer" style={{background: '#121f35'}}>
-        <div className=""><small style={{ borderRadius:'5px', textAlign: 'left', fontSize: '20px'}}><a href={details.live}  target="_blank">Live</a></small>  <small style={{ borderRadius:'5px', marginLeft: '200px', fontSize: '20px'}} ><a href={details.github}  target="_blank">GitHub</a></small></div>
+        <div className=""><small style={{ borderRadius:'5px', textAlign: 'left', fontSize: '20px', textDecoration: 'none'}}><a style={{textDecoration: 'none'}} href={details.live}  target="_blank">Live</a></small>  <small style={{ borderRadius:'5px', marginLeft: '200px', fontSize: '20px'}} ><a style={{textDecoration: 'none'}} href={details.github}  target="_blank">GitHub</a></small></div>
         </div>
         
         
